2e1d94dc664c4c76a56dd0b65259c5e508803e43
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-01-04 22:20:04 -0300

    Iniciando estilização e criando mixins

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -16,6 +16,14 @@ export default function Header() {
                 </ul>
             </nav>
 
+            <div className="header-social-media">
+            <ul>
+                <li> <a href="#"> <i> FB </i> </a> </li>
+                <li> <a href="#"> <i> IG </i> </a> </li>
+                <li> <a href="#"> <i> YT </i> </a> </li>
+            </ul>
+            </div>
+
         </header>
     )
 
